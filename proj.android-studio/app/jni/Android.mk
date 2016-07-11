9f0f0dad45db7c8ab7e6255297431c4f4603a6f1
Alfonso Grillo <alfogrillo@gmail.com>
2016-07-11 18:13:00 +0200

    Configured Android Studio project

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -1,34 +1,46 @@
 LOCAL_PATH := $(call my-dir)
-
+ 
 include $(CLEAR_VARS)
-
+ 
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos/audio/include)
-
-LOCAL_MODULE := MyGame_shared
-
+ 
+LOCAL_MODULE := cocos2dcpp_shared
+ 
 LOCAL_MODULE_FILENAME := libMyGame
-
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
-
-# _COCOS_HEADER_ANDROID_BEGIN
-# _COCOS_HEADER_ANDROID_END
-
-
+ 
+MY_FILES_PATH  :=  $(LOCAL_PATH) \
+                   $(LOCAL_PATH)/../../../Classes
+ 
+MY_FILES_SUFFIX := %.cpp %.c
+                        
+# Make does not offer a recursive wildcard function, so here's one:
+rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+     
+# Retrieve all source files in each dir recursively.
+MY_ALL_FILES := $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*.*) )
+MY_ALL_FILES := $(MY_ALL_FILES:$(MY_CPP_PATH)/./%=$(MY_CPP_PATH)%)
+MY_SRC_LIST  := $(filter $(MY_FILES_SUFFIX),$(MY_ALL_FILES))
+MY_SRC_LIST  := $(MY_SRC_LIST:$(LOCAL_PATH)/%=%)
+ 
+# helper function for remove duplicated words in string test
+define uniq =
+  $(eval seen :=)
+  $(foreach _,$1,$(if $(filter $_,${seen}),,$(eval seen += $_)))
+  ${seen}
+endef
+     
+# Retrieve all directory recursively.
+MY_ALL_DIRS := $(dir $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*/) ) )
+MY_ALL_DIRS := $(call uniq,$(MY_ALL_DIRS))
+ 
+# All done, just tell NDK build system.
+LOCAL_SRC_FILES  := $(MY_SRC_LIST)
+LOCAL_C_INCLUDES := $(MY_ALL_DIRS)
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
-
-# _COCOS_LIB_ANDROID_BEGIN
-# _COCOS_LIB_ANDROID_END
-
+ 
 include $(BUILD_SHARED_LIBRARY)
-
+ 
 $(call import-module,.)
-
-# _COCOS_LIB_IMPORT_ANDROID_BEGIN
-# _COCOS_LIB_IMPORT_ANDROID_END
